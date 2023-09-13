a5b7ca3f30fd5afaabd0f82ae8ee6eeeb1edbefa
Steven Bunde <stevenbunde2020@gmail.com>
2023-09-13 15:21:34 -0400

    set up get route for grabbing notes in the db file

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,8 @@ const router = require("express").Router()
 const { v4: uuidv4 } = require("uuid")
 const { readFromFile, writeToFile, readAndAppend, readAndDelete } = require("../helpers/fsUtils")
 
+router.get("/notes", (req, res) => {
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
 module.exports = router;
